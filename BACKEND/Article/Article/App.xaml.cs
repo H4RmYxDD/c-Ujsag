8acc52a7ae0575e64fd732cdbba99b3ec11cac44
namost <oroszlevi07@gmail.com>
2025-11-19 08:38:51 +0100

    backend mappak

---
+++ BACKEND/Article/Article/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Article
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
