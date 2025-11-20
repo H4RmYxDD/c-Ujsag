51b152cd92444ac589ab2a16d52f45e87fc00d69
Hódi Kristóf <kristof.hodi@gmail.com>
2025-11-20 07:53:34 +0100

    connection table

--- BACKEND/Article/ArticleDataBase/ArticleAuthor.cs
+++ BACKEND/Article/ArticleDataBase/ArticleAuthor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace ArticleDataBase
 {
-    internal class ArticleAuthor
+    [PrimaryKey(nameof(AuthorId), nameof(ArticleId))]
+    public class ArticleAuthor
     {
+        public int AuthorId { get; set; }
+        public int ArticleId { get; set; }
+        public Author Author { get; set; }
+        public Article Article { get; set; }
     }
 }
